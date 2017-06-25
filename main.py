9a3378b1f8d880dba6491ab3270caf69a0333bed
SubashSoftwares <lulostomog@ibsats.com>
2017-06-25 18:28:07 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+###The base of this library is python 2.7.x and you can make changes manually to make this run on later versions if possible;
+###This library is made and distributed without any sorts of warrenties.
+import os, system
+try:
+  import win32api
+  import win32clipboard
+  
+except NameError as err:
+  print ("Please Install The Required Modules Or Libraries");
 
+#Some self closing clipboard functions
+
+if __name__=="__main__":
+  print("welcome to Pywin2.0");
