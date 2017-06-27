259733db933455230bce8e74fa77b126b28151d6
SubashSoftwares <lulostomog@ibsats.com>
2017-06-27 09:14:51 +0545

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,16 @@
 ###This library is made and distributed without any sorts of warrenties.
 import os, system
 try:
-  import win32api
+  import win32api as win32
   import win32clipboard
   
 except NameError as err:
   print ("Please Install The Required Modules Or Libraries");
 
 #Some self closing clipboard functions
+def getClipboardData():
+  OpenClipboard()
+  
 
 if __name__=="__main__":
   print("welcome to Pywin2.0");
